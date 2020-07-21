af9b65920f6d3167281ff3975be4d953caa364b1
Xander <axtg@users.noreply.github.com>
2020-07-21 20:26:01 +0200

    Changed Bootstrap for Bulma classes

--- src/components/AlertSuccess.vue
+++ src/components/AlertSuccess.vue
@@ -1,11 +1,7 @@
 <template>
-  <div v-if="form.successful" class="alert alert-success alert-dismissible" role="alert">
-    <button v-if="dismissible" type="button" class="close" aria-label="Close" @click="dismiss">
-      <span aria-hidden="true">&times;</span>
-    </button>
-
+  <div v-if="form.successful" class="message is-success" role="alert">
     <slot>
-      <div v-html="message"/>
+      <div class="message-content" v-html="message"/>
     </slot>
   </div>
 </template>
